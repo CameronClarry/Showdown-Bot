e353b2e5bf17ee7bf67df29e35558b097c233dd7
CameronClarry <cameronclarry@gmail.com>
2017-02-17 18:46:22 -0500

    Preparation for git module
    Updated template, changed gitignores

--- template.js
+++ template.js
@@ -1,28 +1,57 @@
-let self = {js:{},data:{},requiredBy:[],chathooks:{},messagehooks:{},config:{}};
+let self = {js:{},data:{},requiredBy:[],hooks:{},config:{}};
 exports.onLoad = function(module, loadData){
 	self = module;
 	self.js.refreshDependencies();
 	if(loadData){
-
+		self.data = {};
 	}
-
 	self.chathooks = {
-
-	};
-	self.messagehooks = {
-
+		chathook: function(m){
+			if(m && !m.isInit){
+				let text = m.message;
+				if(text[0]==="~"){
+					let command = text.split(" ")[0].trim().toLowerCase().substr(1);
+					let argText = text.substring(command.length+2, text.length);
+					let chatArgs = argText === "" ? [] : argText.split(",");
+					for(let i = 0;i<chatArgs.length;i++){
+						chatArgs[i] = chatArgs[i].trim();
+					}
+					if(commands[command]&&auth&&auth.js&&chat&&chat.js){
+						let rank = auth.js.getEffectiveRoomRank(m, "");
+						let commandToRun = commands[command];
+						if(typeof commandToRun === "string"){
+							commandToRun = commands[commandToRun];
+						}
+						commandToRun(m, chatArgs, rank);
+					}
+				}
+			}
+		}
 	};
 };
+
 exports.onUnload = function(){
 
 };
+
 exports.refreshDependencies = function(){
 
 };
+
 exports.onConnect = function(){
 
 };
 
 let commands = {
 
+}
+
+let defaultConfigs = {
 };
+
+exports.defaultConfigs = defaultConfigs;
+
+let configTypes = {
+};
+
+exports.configTypes = configTypes;
