2fa895dc55efffd0a201652413bda907df5ff5df
CameronClarry <cameronclarry@gmail.com>
2017-11-04 01:05:42 -0400

    Fix for security issue in the ~timer command
    Will now remove all slashes (/) from timer text to prevent possible
    abuse by making the bot execute commands

--- bot_modules/tt/tt.js
+++ bot_modules/tt/tt.js
@@ -830,6 +830,8 @@ let commands = {
 	timer: function(message, args){
 		let room = toRoomId(args[2]) || message.room;
 		let announcement = args[1] || "Timer's up!";
+		announcement = announcement.replace('/', '');
+		info(announcement)
 		let duration = args[0] && /^\d+$/.test(args[0]) && parseInt(args[0]);
 		if(duration>30){
 			duration = 0;
