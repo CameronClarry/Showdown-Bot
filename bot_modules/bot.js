65b19bb4087b191fad00e7c97cb819ae85fb3365
Cameron Clarry <cameronclarry@gmail.com>
2020-11-16 15:01:39 -0500

    Add main bot module for config purposes

---
+++ bot_modules/bot.js
@@ -0,0 +1,23 @@
+class Bot extends BaseModule{
+	constructor(){
+		super();
+		this.config = {
+			"user": new ConfigString(""),
+			"userId": new ConfigString(""),
+			"pass": new ConfigString(""),
+			"owner": new ConfigString(""),
+			"connection": new ConfigString("ws://sim.smogon.com/showdown/websocket"),
+			"log_receive": new ConfigBoolean(true),
+			"log_send": new ConfigBoolean(true),
+			"dbuser": new ConfigString(""),
+			"dbpassword": new ConfigString(""),
+			"dbhost": new ConfigString(""),
+			"dbport": new ConfigInt(0),
+			"dbname": new ConfigString(""),
+			"text_directory": new ConfigString("./"),
+			"text_web_directory": new ConfigString("www.example.com")
+		};
+	}
+}
+
+exports.Module = Bot
