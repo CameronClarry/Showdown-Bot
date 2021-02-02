c0a8b53778f5718060d4477ccef8d69a42b9defa
Cameron Clarry <cameronclarry@gmail.com>
2021-02-02 11:29:56 -0500

    Fix regular users being able to undo more than one turn

--- bot_modules/minigames.js
+++ bot_modules/minigames.js
@@ -327,7 +327,7 @@ class TriviaTrackerGame{
 	}
 
 	cantNo(user, rank, number){
-		if(!AuthManager.rankgeq(rank, '+') && user.id !== this.curHist.active.id && user.id !== this.host.id) return "Your rank is not high enough to use that command.";
+		if(!AuthManager.rankgeq(rank, '+') && (user.id !== this.curHist.active.id || number > 1) && user.id !== this.host.id) return "Your rank is not high enough to use that command.";
 		
 		if(this.lastNo && Date.now() - this.lastNo < 5000) return "There is a cooldown between uses of ~no, try again in a few seconds.";
 
